d5e3f3cc960998259cab3065af5ee9445a62b226
manish <130219030+Manishreddy007@users.noreply.github.com>
2024-09-04 12:18:04 +0000

    03

---
+++ 01_basics/03_conversionsAndOperations.js
@@ -0,0 +1,14 @@
+// let score = "33asdad"
+
+// let scoreInNumber = Number(score)
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber)
+
+// let num1 = 33
+// let str1 = String(num1)
+
+// console.log(str1)
+// console.log(typeof str1);
+
+//******operations */
+
